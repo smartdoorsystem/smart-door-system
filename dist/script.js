bc319a9830b553f4adfdcda9c8b33aaa00888f3a
smartdoorsystem <smart.door.system.project@gmail.com>
2025-09-06 05:25:28 +0200

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -20,40 +20,16 @@ $(function() {
 });
 
 $(function() {
-	$(".btn-signin").click(function() {
-  const username = $(".form-signin input[name='username']").val();
-  const password = $(".form-signin input[name='password']").val();
+  $(".btn-signin").click(function() {
+    const username = $(".form-signin input[name='username']").val();
+    const password = $(".form-signin input[name='password']").val();
 
-  if (username === "smart_door_system" && password === "1037") {
-    window.location.href = "abdelraouf.html";
-  } else {
-    alert("Incorrect username or password. Please try again.");
-    $(".btn-animate").toggleClass("btn-animate-grow");
-    $(".welcome").toggleClass("welcome-left");
-    $(".cover-photo").toggleClass("cover-photo-down");
-    $(".frame").toggleClass("frame-short");
-    $(".profile-photo").toggleClass("profile-photo-down");
-    $(".btn-goback").toggleClass("btn-goback-up");
-    $(".forgot").toggleClass("forgot-fade");
-  }
-	});
-});
-
-$(function() {
-  $("#forgot-password-link").click(function(e) {
-    e.preventDefault();
-    alert("Hint: previous computer password");
+    if (username === "smart_door_system" && password === "1037") {
+      window.location.href = "abdelraouf.html";
+    } else {
+      alert("Incorrect username or password. Please try again.");
+    }
   });
-});
-    $(".btn-animate").toggleClass("btn-animate-grow");
-    $(".welcome").toggleClass("welcome-left");
-    $(".cover-photo").toggleClass("cover-photo-down");
-    $(".frame").toggleClass("frame-short");
-    $(".profile-photo").toggleClass("profile-photo-down");
-    $(".btn-goback").toggleClass("btn-goback-up");
-    $(".forgot").toggleClass("forgot-fade");
-  }
-	});
 });
 
 $(function() {
