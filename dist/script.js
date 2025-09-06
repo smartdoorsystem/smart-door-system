e4812ef131058120475417659641a57655113b93
smartdoorsystem <smart.door.system.project@gmail.com>
2025-09-06 05:20:49 +0200

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -12,9 +12,6 @@ $(function() {
 
 $(function() {
 	$(".btn-signup").click(function() {
-        // Since we changed the button type to submit, this block is not needed for the form to submit.
-        // It's here for animation, but the form will handle the data submission to Formspree.
-        // You can remove this block if it causes any issues.
   $(".nav").toggleClass("nav-up");
   $(".form-signup-left").toggleClass("form-signup-down");
   $(".success").toggleClass("success-left"); 
@@ -28,12 +25,26 @@ $(function() {
   const password = $(".form-signin input[name='password']").val();
 
   if (username === "smart_door_system" && password === "1037") {
-    // Correct credentials, redirect to abdelraouf.html
     window.location.href = "abdelraouf.html";
   } else {
-    // Incorrect credentials, show an alert
     alert("Incorrect username or password. Please try again.");
-    // Optional: Keep the animation but prevent redirection.
+    $(".btn-animate").toggleClass("btn-animate-grow");
+    $(".welcome").toggleClass("welcome-left");
+    $(".cover-photo").toggleClass("cover-photo-down");
+    $(".frame").toggleClass("frame-short");
+    $(".profile-photo").toggleClass("profile-photo-down");
+    $(".btn-goback").toggleClass("btn-goback-up");
+    $(".forgot").toggleClass("forgot-fade");
+  }
+	});
+});
+
+$(function() {
+  $("#forgot-password-link").click(function(e) {
+    e.preventDefault();
+    alert("Hint: previous computer password");
+  });
+});
     $(".btn-animate").toggleClass("btn-animate-grow");
     $(".welcome").toggleClass("welcome-left");
     $(".cover-photo").toggleClass("cover-photo-down");
